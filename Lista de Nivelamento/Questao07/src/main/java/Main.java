04f183f1379d2e51e27cc8fe1222a9d6f8290b8f
joaoffnascimento <joao.nascimento98@academico.ifs.edu.br>
2018-01-29 17:17:29 -0300

    Questao07

---
+++ Lista de Nivelamento/Questao07/src/main/java/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner read = new Scanner(System.in);
+        System.out.println("Digite um número para A:");
+        int a = read.nextInt();
+        System.out.println("Digite um número para B:");
+        int b = read.nextInt();
+
+    }
+}
