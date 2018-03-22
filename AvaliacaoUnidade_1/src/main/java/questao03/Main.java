f5389272d1d304ece1cf642dec13167252dfef0c
joaoffnascimento <joao.nascimento98@academico.ifs.edu.br>
2018-03-22 20:51:37 -0300

    Questao03 + - respondida

--- AvaliacaoUnidade_1/src/main/java/questao03/Main.java
+++ AvaliacaoUnidade_1/src/main/java/questao03/Main.java
@@ -1,29 +1,68 @@
 package questao03;
 
+import java.util.Random;
 import java.util.Scanner;
 
-public class Main {
+enum Tipo{
 
-    public static final int TAMANHO_FILA = 10;
+    MaiorQZeroPar,
+    MaiorQZeroImpar,
+    MenorIgualZeroPar,
+    MenorIgualZeroImpar
+}
 
+abstract class QuestaoProva {
+
+    public abstract void adicionar (int valor);
+    public abstract int obterProximo (Tipo tipo);
+    public abstract void excluirTodos(Tipo tipo);
+    int capacidadeMaxPorTipo = 0;
+
+    public QuestaoProva (int capacidadeMaxPorTipo){
+        this.capacidadeMaxPorTipo = capacidadeMaxPorTipo;
+
+    }
+    public QuestaoProva(){
+    //Construtor padrao.
+    }
+}
+
+
+
+public abstract class Main extends QuestaoProva{
+
+    public static final int TAMANHO_FILA = 50;
+
+    public static int gerarNumero(){
+        Random random = new Random();
+        
+
+    }
     public static void main(String[] args) {
+
         Scanner read = new Scanner(System.in);
+
+        Fila f1 = Fila.createFilaFactory(TAMANHO_FILA);
+        Fila f2 = Fila.createFilaFactory(TAMANHO_FILA);
+        Fila f3 = Fila.createFilaFactory(TAMANHO_FILA);
+        Fila f4 = Fila.createFilaFactory(TAMANHO_FILA);
 
         int menu = -1;
 
         do {
             System.out.println("==================== MENU ====================");
-            System.out.println("1- ");
-
+            System.out.println("1- Preencher estrutura de dados.");
             System.out.println("0- Sair.");
             menu = read.nextInt();
 
             switch (menu) {
                 case 1:
+                    for (int i = 0; i < 50; i++) {
 
+                    }
                     break;
                 case 2:
-
+                    System.out.println(f1.tamanho());
                     break;
                 case 3:
 
