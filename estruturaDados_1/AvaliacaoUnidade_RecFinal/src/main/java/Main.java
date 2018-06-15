5e1ef79e299f2267658072e41656ebdef8be6cce
joaoffnascimento <thecurrentusergta@gmail.com>
2018-06-14 21:14:51 -0300

    Metade da questao02

--- estruturaDados_1/AvaliacaoUnidade_RecFinal/src/main/java/Main.java
+++ estruturaDados_1/AvaliacaoUnidade_RecFinal/src/main/java/Main.java
@@ -42,7 +42,49 @@ public class Main {
         }
     }
 
+    public static IPilha diferencaInterseccao(IPilha pilhaA, IPilha pilhaB) {
+        Pilha aux1 = new Pilha();
+        Pilha aux2 = new Pilha();
+        IPilha aux3 = new Pilha();
+        IPilha aux4 = new Pilha();
+
+
+        Pilha pilhaSaida = new Pilha();
+        aux1.inicializarPilha(pilhaA.size());
+        aux2.inicializarPilha(pilhaB.size());
+
+        int diferente = 0;
+
+        int tamPilhaA = pilhaA.size();
+        int tamPilhaB = pilhaB.size();
+        pilhaSaida.inicializarPilha(tamPilhaA + tamPilhaB);
+
+        while (!pilhaA.isEmpty()) {
+            Object dadoB;
+            Object dadoA = pilhaA.pop();
+            aux1.push(dadoA);
+            for (int i = 0; i < tamPilhaB; i++) {
+                dadoB = pilhaB.pop();
+                aux2.push(dadoB);
+                if (dadoA.equals(dadoB)) {
+                    diferente++;
+                }
+            }
+
+            if (diferente == 0) {
+                pilhaSaida.push(dadoA);
+            }
+            do {
+                pilhaB.push(aux2.pop());
+            } while (!aux2.isEmpty());
+
+            diferente = 0;
+        }
+        return pilhaSaida;
+    }
+
     public static void main(String[] args) {
+        System.out.println("TESTE PRIMEIRA QUESTAO");
         Pilha pilhaUm = new Pilha();
         Pilha pilhaDois = new Pilha();
 
@@ -53,16 +95,16 @@ public class Main {
 
 
         int a = 0;
-        do{
+        do {
             pilhaUm.push(a);
             a++;
-        }while (!pilhaUm.isFull());
+        } while (!pilhaUm.isFull());
 
         int b = 0;
-        do{
+        do {
             pilhaDois.push(b);
             b++;
-        }while (!pilhaDois.isFull());
+        } while (!pilhaDois.isFull());
 
 
         System.out.println("Pilhas antes da comparacao = ");
@@ -82,16 +124,16 @@ public class Main {
 
 
         a = 0;
-        do{
+        do {
             pilhaUm.push(a);
             a++;
-        }while (!pilhaUm.isFull());
+        } while (!pilhaUm.isFull());
 
         b = 0;
-        do{
+        do {
             pilhaDois.push(b);
             b++;
-        }while (!pilhaDois.isFull());
+        } while (!pilhaDois.isFull());
 
 
         System.out.println("Pilhas antes da comparacao = ");
@@ -103,7 +145,33 @@ public class Main {
         System.out.println("Pilhas depois da comparacao = ");
         System.out.println(pilhaUm);
         System.out.println(pilhaDois);
+        System.out.println("-------------------------------------------------------");
+        System.out.println("TESTE SEGUNDA QUESTAO");
+        Pilha pilhaTres = new Pilha();
+        Pilha pilhaQuatro = new Pilha();
 
+        pilhaTres.inicializarPilha(10);
+        pilhaQuatro.inicializarPilha(10);
 
+        int c = 0;
+        do {
+            pilhaTres.push(c);
+            c++;
+        } while (!pilhaTres.isFull());
+
+        int d = 5;
+        do {
+            pilhaQuatro.push(d);
+            d++;
+        } while (!pilhaQuatro.isFull());
+
+        System.out.println("Pilha 1 = ");
+        System.out.println(pilhaTres);
+        System.out.println("Pilha 2 = ");
+        System.out.println(pilhaQuatro);
+        IPilha pilhaDiferencaInterceccao;
+        pilhaDiferencaInterceccao = diferencaInterseccao(pilhaTres, pilhaQuatro);
+        System.out.println("!INTERCECCAO === ");
+        System.out.println(pilhaDiferencaInterceccao);
     }
 }
