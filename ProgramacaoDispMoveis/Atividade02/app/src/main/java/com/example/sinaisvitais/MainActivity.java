9c53d106da8190a926044f96ea75ee7ae372191c
joaoffnascimento <thecurrentusergta@gmail.com>
2019-03-20 18:04:33 -0300

    disp moveis atividade

---
+++ ProgramacaoDispMoveis/Atividade02/app/src/main/java/com/example/sinaisvitais/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.sinaisvitais;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onClick(View view){
+        Toast.makeText(MainActivity.this, "Salvo!", Toast.LENGTH_LONG);
+    }
+}
