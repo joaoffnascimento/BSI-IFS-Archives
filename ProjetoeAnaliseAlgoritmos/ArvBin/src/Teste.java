b213a02356a073256672b663757f301cc54e2236
joaoffnascimento <thecurrentusergta@gmail.com>
2019-05-16 21:00:23 -0300

    alt

---
+++ ProjetoeAnaliseAlgoritmos/ArvBin/src/Teste.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class Teste {
+    public static void main(String[] args) {
+        Scanner read = new Scanner(System.in);
+        ArrayList<String> estacionamento = new ArrayList<>();
+        String[] est = new String[500];
+        String[] esp = new String[500];
+        String atual = "";
+        char op = ' ';
+        int count = 0;
+
+        do{
+            atual = read.next();
+            op = read.nextLine().charAt(0);
+
+            if()
+
+            count ++;
+        }while (count <= 11);
+    }
+}
+
+// String tokenizer
+// Hash set
